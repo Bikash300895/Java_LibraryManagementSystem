cfa13c4ce4347c381842e208ee4002ac954cd9ee
Bikash <bikash11roy@gmail.com>
2017-07-16 07:37:52 +0600

    New book addition page redirected

--- src/Home.java
+++ src/Home.java
@@ -189,6 +189,9 @@ public class Home extends javax.swing.JFrame {
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         // TODO add your handling code here:
+        NewBook ob = new NewBook();
+        setVisible(false);
+        ob.setVisible(true);
     }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
