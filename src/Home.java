d68fc5ee961f585e6f130c62bddb14f2e3acfe1a
Bikash <bikash11roy@gmail.com>
2017-07-16 11:05:55 +0600

    Issue page linked from home page

--- src/Home.java
+++ src/Home.java
@@ -85,6 +85,11 @@ public class Home extends javax.swing.JFrame {
         });
 
         jButton4.setIcon(new javax.swing.ImageIcon("D:\\PROJECTS\\Java_LibraryManagementSystem\\images\\issue.png")); // NOI18N
+        jButton4.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton4ActionPerformed(evt);
+            }
+        });
 
         jLabel3.setText("jLabel3");
 
@@ -267,6 +272,13 @@ public class Home extends javax.swing.JFrame {
         setVisible(false);
         ob.setVisible(true);
     }//GEN-LAST:event_jButton3ActionPerformed
+
+    private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
+        // TODO add your handling code here:
+        Issue ob = new Issue();
+        setVisible(false);
+        ob.setVisible(true);
+    }//GEN-LAST:event_jButton4ActionPerformed
 
     /**
      * @param args the command line arguments
